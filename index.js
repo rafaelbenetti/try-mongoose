7ef3c9dff9ab48fe0f10aa1d4586dbb7d0d4160a
Rafael de Oliveira Benetti <rafaelbenetti@cwi.com.br>
2017-07-18 18:49:33 -0300

    config(mongoose): change mongoose default's promise

--- index.js
+++ index.js
@@ -3,15 +3,18 @@
 
     const express = require('express');
     const mongoose = require('mongoose');
+    const bluebird = require('bluebird');
+
     let app = express();
 
+    mongoose.Promise = bluebird;
     mongoose
         .connect('mongodb://database:27017', {
             useMongoClient: true
         })
         .then((db) => {
             console.log('Connected on database!!');
-        });
+        });    
 
     app.get('/', (req, res) => res.send("Hello World"));
     app.listen(3000, () => console.log('Listening on port 3000!'));
